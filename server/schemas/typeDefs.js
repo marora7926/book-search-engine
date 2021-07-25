1667d22178824679c4436c9ae05c843a87e1e12f
Mohit Arora <mohit.arora@sydney.edu.au>
2021-07-26 02:41:31 +1000

    added typeDefs and apollo-client to client side

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,39 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+type Book {
+    _id: ID!
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+}
 
+type User {
+    _id: ID!
+    username: String
+    email: String
+    password: String
+    savedBooks: [Book]
+}
+
+type Query {
+    me: User
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: savedBook!): User
+    removeBook(bookId: ID!): User
+}
+
+type Auth {
+    token: ID!
+    user: User
+  }
 `;
 
 module.exports = typeDefs;
