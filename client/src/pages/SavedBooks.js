9fddc8ceefc1c11067abbbd482cad9009a3016c5
Mohit Arora <mohit.arora@sydney.edu.au>
2021-07-27 16:51:43 +1000

    added add_book and remove_book mutations

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
-// import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 import { REMOVE_BOOK } from '../utils/mutations';
-import { GET_ME } from '../utils/queries';
+import { QUERY_ME } from '../utils/queries';
 
 const SavedBooks = () => {
   const [userData, setUserData] = useState({});
--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { useMutation } from '@apollo/client';
+import { ADD_BOOK } from '../../utils/mutations';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -64,6 +64,18 @@ const SearchBooks = () => {
       return false;
     }
 
+    try {
+      const data = await saveBook({
+        variables: { 
+          profileId, 
+          skill },
+      });
+
+      setSkill('');
+    } catch (err) {
+      console.error(err);
+    }
+    
     try {
       const response = await saveBook(bookToSave, token);
 
